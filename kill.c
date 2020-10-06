af043921d728f6703763f544e63a1654bdabb591
Jakob Kaivo <jkk@ung.org>
2020-10-06 15:32:01 -0400

    extract kill, as it can also stand alone

--- kill.c
+++ kill.c
@@ -1,7 +1,7 @@
 /*
  * UNG's Not GNU
  *
- * Copyright (c) 2011-2017, Jakob Kaivo <jkk@ung.org>
+ * Copyright (c) 2011-2020, Jakob Kaivo <jkk@ung.org>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -18,99 +18,12 @@
  */
 
 #include "sh.h"
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <unistd.h>
-
-struct {
-	char *name;
-	int num;
-} ksigs[] = {
-	{ "ABRT",	SIGABRT },
-	{ "ALRM",	SIGALRM },
-	{ "BUS",	SIGBUS },
-	{ "CHLD",	SIGCHLD },
-	{ "CONT",	SIGCONT },
-	{ "FPE",	SIGFPE },
-	{ "HUP",	SIGHUP },
-	{ "ILL",	SIGILL },
-	{ "INT",	SIGINT },
-	{ "KILL",	SIGKILL },
-	{ "PIPE",	SIGPIPE },
-	{ "QUIT",	SIGQUIT },
-	{ "SEGV",	SIGSEGV },
-	{ "STOP",	SIGSTOP },
-	{ "TERM",	SIGTERM },
-	{ "TSTP",	SIGTSTP },
-	{ "TTIN",	SIGTTIN },
-	{ "TTOU",	SIGTTOU },
-	{ "USR1",	SIGUSR1 },
-	{ "USR2",	SIGUSR2 },
-	{ "POLL",	SIGPOLL },
-	{ "PROF",	SIGPROF },
-	{ "SYS",	SIGSYS },
-	{ "TRAP",	SIGTRAP },
-	{ "URG",	SIGURG },
-	{ "VTALRM",	SIGVTALRM },
-	{ "XCPU",	SIGXCPU },
-	{ "XFSZ",	SIGXFSZ },
-};
-size_t nsigs = sizeof(ksigs) / sizeof(ksigs[0]);
+#define main real_kill_main
+#include "kill/kill.c"
 
 int kill_main(int argc, char *argv[])
 {
-	char *signame = "TERM";
-	int list = 0;
-
+	/* TODO: translate %job to pid */
 	opterr = 0;
-	int c;
-	while ((c = getopt(argc, argv, "s:l")) != -1) {
-		switch (c) {
-		case 's':	/** send signal [signal_name] **/
-			signame = optarg;
-			break;
-
-		case 'l':
-			list = 1;
-			break;
-
-		default:
-			if (isdigit(optopt) || isupper(optopt)) {
-				signame = argv[optind];
-				break;
-			}
-			fprintf(stderr, "kill: unknown option '-%c'\n", optopt);
-			//return 1;
-		}
-	}
-
-	if (list) {
-		if (argv[optind]) {
-		}
-
-		for (size_t i = 0; i < nsigs; i++) {
-			printf("%s%c", ksigs[i].name, i < nsigs - 1 ? ' ' : '\n');
-		}
-		return 0;
-	}
-
-	if (argc <= optind) {
-		fprintf(stderr, "kill: missing operand\n");
-		return 1;
-	}
-
-	int pid = atoi(argv[optind]);
-	for (size_t i = 0; i < nsigs; i++) {
-		if (!strcmp(signame, ksigs[i].name)) {
-			if (kill(pid, ksigs[i].num) != 0) {
-				perror("kill");
-			}
-		}
-	}
-
-	fprintf(stderr, "kill: invalid signal '%s'\n", signame);
-	return 1;
+	return real_kill_main(argc, argv);
 }
